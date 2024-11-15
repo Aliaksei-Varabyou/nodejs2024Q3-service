035171bfee72f223211b06088bd5b9f2d50c2610
Aliaksei Varabyou <aliaksei_varabyou@epam.com>
2024-11-15 16:58:41 +0100

    docker and postgres setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,8 +12,12 @@ import { FavModule } from './Favorite/fav.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: ':memory:',
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [join(__dirname, '**', '*.entity{.ts,.js}')],
       synchronize: true,
       logging: true,
